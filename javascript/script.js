7189bf9995ee15e3c98444c57218dbbb322a3078
FonesArielDW <fonesarieldw@gmail.com>
2024-03-20 18:52:45 -0300

    miercoles

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
-    var height = $('.footer').height();
-    $('.sect-t-footer').css('height', height);
+    $('.banner__scroll-down').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.sect--type ').offset().top
+        }, 1000); // 1000 es la duración de la animación en milisegundos
+    });
 });
